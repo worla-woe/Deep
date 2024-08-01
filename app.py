1418b37f47d1304bd49eba9a62eccc89dae6c2f7
worla-woe <160612609+worla-woe@users.noreply.github.com>
2024-08-01 22:26:52 +0000

    Update app.py crossorigin

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5174"],  # Allow your frontend's origin
+    allow_origins=["https://phishing-detection-react-e95c9.web.app"],  # Allow your frontend's origin
     allow_credentials=True,
     allow_methods=["*"],  # Allow all methods
     allow_headers=["*"],  # Allow all headers
